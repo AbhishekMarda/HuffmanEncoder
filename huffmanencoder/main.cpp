28f87d981cba32102af5c3edb72db06dccd9c1f9
AbhishekMarda <abhishek1marda@ucla.edu>
2020-06-12 19:31:54 -0500

    Initial Commit

---
+++ huffmanencoder/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  huffmanencoder
+//
+//  Created by Abhishek Marda on 6/12/20.
+//  Copyright © 2020 Abhishek Marda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
